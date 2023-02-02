ac2fff91d02cdeceb9a8adff5deefecf42b6a254
HuoKnight <noman.gronckle@gmail.com>
2023-02-01 16:09:35 -0800

    Working ID system and remove button

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from tkinter import *
 
     # TODO: 
     # - Create ID system so that each visual rectangle has an incremented ID, and rectangles that are created and then deleted
-    #   during resizing do not effect the ID count.
+    #   during resizing do not effect the ID count. | DONE
     # - Include this for all shapes
-    # - Add remove tool
-    # - Add custom cursors to canvas
+    # - Add remove tool | DONE
+    # - Add custom cursors to canvas; Fleur for shape resizing, pirate for removing, spraycan for color filling
 
 class EasyCMUCanvas():
     def __init__(self):
@@ -14,7 +14,8 @@ class EasyCMUCanvas():
         self.root.resizable(False, False)
         self.tool = 'rect'
         self.rect = None
-        self.current_id = 0
+        self.oval = None
+        self.current_tag = -1
 
 
         self.layout = Frame(self.root)
@@ -22,7 +23,7 @@ class EasyCMUCanvas():
         self.button_layout = Frame(self.layout)
         self.button_layout.grid(row=1, column=0)
 
-        self.canvas = Canvas(self.layout, bg="white", height=400, width=400)
+        self.canvas = Canvas(self.layout, bg="white", height=400, width=400, cursor='fleur')
         self.canvas.bind("<Button-1>", self.getMousePos)
         self.canvas.bind("<B1-Motion>", self.mouseDrag)
         self.canvas.bind("<ButtonRelease-1>", self.mouseRelease)
@@ -43,23 +44,37 @@ class EasyCMUCanvas():
         self.start_x = event.x
         self.start_y = event.y
         if self.tool != 'remove':
-            self.current_id += 1
+            self.current_tag += 1
+        else:
+            for shape_id in [id for id in range(self.current_tag + 1)]:
+                tag = 'a' + str(shape_id)
+                print(tag, ';', self.current_tag)
+                if self.collides(event.x, event.y, self.canvas.coords(tag)):
+                    self.canvas.delete(tag)
+                    break
 
     def mouseDrag(self, event):
         if self.tool == 'rect':
             if self.rect:
-                self.current_id -= 1
-                print(self.current_id, ';', self.rect)
-                self.canvas.delete(self.rect)
+                print(self.current_tag, ';', self.canvas.coords('a' + str(self.current_tag)))
+                self.canvas.delete('a' + str(self.current_tag))
             current_x = event.x
             current_y = event.y
-            self.current_id += 1
-            self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, current_x, current_y, tag=self.current_id)
+            self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, current_x, current_y, tags='a' + str(self.current_tag))
             
     
     def mouseRelease(self, event):
         if self.tool == 'rect':
             self.rect = None
+
+    def collides(self, mouse_x, mouse_y, coords):
+        print(coords, ';', mouse_x, ',', mouse_y)
+        if coords != []:
+            if (mouse_x >= coords[0] and mouse_x <= coords[2]) and (mouse_y >= coords[1] and mouse_y <= coords[3]):
+                return True
+        return False
+        
+
         
     
 
