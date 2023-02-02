dd6bc9f13c4b7a36159baf50f66739702cff9b27
HuoKnight <noman.gronckle@gmail.com>
2023-02-01 16:09:35 -0800

    Ovals, polygon selection, assorted bits and bobs

--- src/main.py
+++ src/main.py
@@ -9,21 +9,31 @@ from tkinter import colorchooser
     # - Add all shapes
     # - Include id system for all shapes
     # - Add remove tool | DONE
-    # - Add custom cursors to canvas; Fleur for shape resizing, pirate for removing, spraycan for color filling
+    # - Add custom cursors to canvas; Fleur for shape resizing, pirate for removing, spraycan for color filling | Done
     # - Add fill button | DONE
-    # - Make button images work | Done
+    # - Make button images work | DONE
+    # - Add ability to create shapes at specific coordinates
 
 class EasyCMUCanvas():
     def __init__(self):
         self.root = Tk()
         self.root.wm_title("Easy CMU Canvas")
         self.root.resizable(False, False)
+        self.tooltip = Toplevel(self.root)
+        self.tooltip.withdraw()
+        self.tooltip.overrideredirect(True)
+        self.tooltip_label = Label(self.tooltip, text="", background="lightyellow", relief="solid", borderwidth=1)
+        self.tooltip_label.pack()
         self.tool = 'shape'
         self.rect = None
         self.oval = None
         self.current_tag = -1
         self.shape = 'rect'
         self.color = '#000000'
+
+        self.polygonMenu = Menu(self.root, tearoff=0)
+        self.polygonMenu.add_command(label='Rect', command=lambda: self.choosePolygon('rect'))
+        self.polygonMenu.add_command(label='Oval', command=lambda: self.choosePolygon('oval'))
 
         self.fill_bucket_photo = PhotoImage(file='./img/fill_bucket.png')
         self.gradient_photo = PhotoImage(file='./img/gradient.png')
@@ -39,16 +49,38 @@ class EasyCMUCanvas():
         self.canvas.bind("<ButtonRelease-1>", self.mouseRelease)
         self.canvas.grid(row=1, column=1)
 
-        self.rectButton = Button(self.button_layout, text='▮', font=('Arial', 25), width=1, height=1, command=lambda: self.selectTool('shape')).grid(row=0, column=0)   
-        self.removeButton = Button(self.button_layout, text='✕', fg='red', font=('Arial', 25), width=1, height=1, command=lambda: self.selectTool('remove')).grid(row=1, column=0)
-        self.fillButton = Button(self.button_layout, image=self.fill_bucket_photo, font=('Arial', 25), width=41, height=40, command=lambda: self.selectTool('fill')).grid(row=2, column=0)
-        self.colorButton = Button(self.button_layout, image=self.gradient_photo, font=('Arial', 25), width=41, height=40, command=lambda: self.chooseColor()).grid(row=3, column=0)
+        self.shapeButton = Button(self.button_layout, text='★', font=('Arial', 25), width=1, height=1, command=lambda: self.selectTool('shape'))
+        self.shapeButton.bind("<Enter>", lambda event, text='Polygon': self.showTooltip(event, text))
+        self.shapeButton.bind("<Leave>", self.hideTooltip)
+        self.shapeButton.bind("<Button-3>", self.choosePolygonMenu)
+        self.shapeButton.grid(row=0, column=0)
+
+        self.removeButton = Button(self.button_layout, text='✕', fg='red', font=('Arial', 25), width=1, height=1, command=lambda: self.selectTool('remove'))
+        self.removeButton.bind("<Enter>", lambda event, text='Delete': self.showTooltip(event, text))
+        self.removeButton.bind("<Leave>", self.hideTooltip)
+        self.removeButton.grid(row=1, column=0)
+
+        self.fillButton = Button(self.button_layout, image=self.fill_bucket_photo, font=('Arial', 25), width=41, height=40, command=lambda: self.selectTool('fill'))
+        self.fillButton.bind("<Enter>", lambda event, text='Fill': self.showTooltip(event, text))
+        self.fillButton.bind("<Leave>", self.hideTooltip)
+        self.fillButton.grid(row=2, column=0)
+
+        self.colorButton = Button(self.button_layout, image=self.gradient_photo, font=('Arial', 25), width=41, height=40, command=lambda: self.chooseColor())
+        self.colorButton.bind("<Enter>", lambda event, text='Color Picker': self.showTooltip(event, text))
+        self.colorButton.bind("<Leave>", self.hideTooltip)
+        self.colorButton.grid(row=3, column=0)
 
         
         
 
         self.root.mainloop()
 
+
+    def choosePolygonMenu(self, event):
+        self.polygonMenu.post(event.x_root, event.y_root)
+    
+    def choosePolygon(self, shape):
+        self.shape = shape
 
     def chooseColor(self):
         self.color = colorchooser.askcolor(title='Color Picker')[1]
@@ -79,19 +111,20 @@ class EasyCMUCanvas():
             for shape_id in [id for id in range(self.current_tag + 1)]:
                 tag = 'a' + str(shape_id)
                 if self.collides(event.x, event.y, self.canvas.coords(tag)):
-                    shape_size = self.canvas.coords(tag)
-                    self.canvas.delete(tag)
-                    self.canvas.create_rectangle(shape_size[0], shape_size[1], shape_size[2], shape_size[3], tags=tag, fill=self.color)
+                    self.fillShape(self.canvas.type(tag), self.canvas.coords(tag), tag)
                     break
 
     def mouseDrag(self, event):
         if self.tool == 'shape':
-            if self.rect:
-                print(self.current_tag, ';', self.canvas.coords('a' + str(self.current_tag)))
-                self.canvas.delete('a' + str(self.current_tag))
-            current_x = event.x
-            current_y = event.y
-            self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, current_x, current_y, tags='a' + str(self.current_tag))
+            if self.shape == 'rect':
+                if self.rect:
+                    print(self.current_tag, ';', self.canvas.coords('a' + str(self.current_tag)))
+                    self.canvas.delete('a' + str(self.current_tag))
+                self.rect = self.canvas.create_rectangle(self.start_x, self.start_y, event.x, event.y, tags='a' + str(self.current_tag))
+            elif self.shape == 'oval':
+                if self.oval:
+                    self.canvas.delete('a' + str(self.current_tag))
+                self.oval = self.canvas.create_oval(self.start_x, self.start_y, event.x, event.y, tags='a' + str(self.current_tag))
             
     
     def mouseRelease(self, event):
@@ -104,6 +137,32 @@ class EasyCMUCanvas():
             if (mouse_x >= coords[0] and mouse_x <= coords[2]) and (mouse_y >= coords[1] and mouse_y <= coords[3]):
                 return True
         return False
+
+
+    def showTooltip(self, event, text):
+        cx, cy = event.x_root, event.y_root
+        self.tooltip_label.config(text=text)
+        self.tooltip.wm_geometry("+%d+%d" % (cx + 10, cy + 10))
+        self.tooltip.update()
+        self.tooltip.deiconify()
+    
+    def hideTooltip(self, event):
+        self.tooltip.withdraw()
+    
+    def fillShape(self, shape, coords, tag):
+        self.canvas.delete(tag)
+        if shape == 'rectangle':
+            if self.color == '#ffffff':
+                self.canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], tags=tag, fill=self.color)
+            else:
+                self.canvas.create_rectangle(coords[0], coords[1], coords[2], coords[3], tags=tag, fill=self.color, width=0)
+        elif shape == 'oval':
+            if self.color == '#ffffff':
+                self.canvas.create_oval(coords[0], coords[1], coords[2], coords[3], tags=tag, fill=self.color)
+            else:
+                self.canvas.create_oval(coords[0], coords[1], coords[2], coords[3], tags=tag, fill=self.color, width=0)
+
+
         
 
         
